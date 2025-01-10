13b2a4e08ebafaa8c5322f3eebd9fd6f732b7665
sanushifan <sanushifankc@gmail.com>
2025-01-10 20:17:54 +0530

    remove env file from commit

--- middlewere/user_auth.js
+++ middlewere/user_auth.js
@@ -2,11 +2,8 @@ const express = require("express");
 
 module.exports = user_token_check = (req, res, next) => {
   if (req.session.user_token) {
-    // req.session.user_token = "67349af1c25d08422ae580d3";
     next();
   } else {
-    // req.session.user_token = "67349af1c25d08422ae580d3";
-    next();
     res.redirect("/")
   }
 };
