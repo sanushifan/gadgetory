9ee7dd91bee89d1da67712a258c8f728702d044b
sanushifan <sanushifankc@gmail.com>
2025-01-11 20:08:31 +0530

    PORT setups

--- main.js
+++ main.js
@@ -39,4 +39,4 @@ app.get('*',(req,res)=>{
 
 // Server Initialization
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT , '0.0.0.0' , () => console.log(`Server running on port ${PORT}`));
