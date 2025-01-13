4156514ef500d58184942ee771afa98148b2a361
sanushifan <sanushifankc@gmail.com>
2025-01-13 10:33:16 +0530

    google redirect url chenges

--- controllers/user/external_authentication.js
+++ controllers/user/external_authentication.js
@@ -15,7 +15,7 @@ passport.use(
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback",
+        callbackURL: "http://gadgetory.shop/auth/google/callback",
       },
       (accessToken, refreshToken, profile, done) => {
         return done(null, profile);
